a7d7012110e4058f34d40712ce2ed1cdd25cef28
Sibiraj M <sibiraj.github@gmail.com>
2026-01-04 14:01:26 +0530

    All_Gates

--- 1.Gates/015.All_Gates.v
+++ 1.Gates/015.All_Gates.v
@@ -11,3 +11,10 @@ assign xor_out=a^b;
 assign xnor_out=~(a^b);
 
 endmodule
+
+//OUTPUT
+a=0 b=0 | AND=0 OR=0 NOT=1 NAND=1 NOR=1 XOR=0 XNOR=1
+a=0 b=1 | AND=0 OR=1 NOT=1 NAND=1 NOR=0 XOR=1 XNOR=0
+a=1 b=0 | AND=0 OR=1 NOT=0 NAND=1 NOR=0 XOR=1 XNOR=0
+a=1 b=1 | AND=1 OR=1 NOT=0 NAND=0 NOR=0 XOR=0 XNOR=1
+All_Gates_tb.v:27: $finish called at 40 (1s)
