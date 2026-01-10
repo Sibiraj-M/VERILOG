65aab9689e3d0f3a2fdfec53ab20c5991b9fc3cb
Sibiraj M <sibiraj.github@gmail.com>
2026-01-10 10:58:29 +0530

    Update 026.half subtractor using behavioral modeling.v

--- 2.Combinational_Circuits_1/026.half subtractor using behavioral modeling.v
+++ 2.Combinational_Circuits_1/026.half subtractor using behavioral modeling.v
@@ -4,3 +4,9 @@ module half_sub(input a,b,output reg diff,borrow);
     borrow = ~a&b;
   end
 endmodule
+
+//output
+a=0 b=0 | diff=0 borrow=0
+a=0 b=1 | diff=1 borrow=1
+a=1 b=0 | diff=1 borrow=0
+a=1 b=1 | diff=0 borrow=0
